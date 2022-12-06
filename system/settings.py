740dfbde1796fd98e4fa2f0dd634846d40cc4388
Brayan opiyo <67815971+opiyobrayan@users.noreply.github.com>
2022-12-06 13:58:32 +0300

    Update settings.py

--- system/settings.py
+++ system/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,6 +128,7 @@ STATICFILES_DIR=[
 ]
 MEDIA_URL = "/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"static/media/")
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
